77da5c71afe437ecb8336cf68533679f64096258
Gordon D <gwd2018@gmail.com>
2022-10-28 16:01:57 -0400

    added basic loading page with static gif

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,21 @@ class MainApp extends StatelessWidget {
             ),
           );
         }
-        return const Text('Loading', textDirection: TextDirection.ltr);
+        return Center(
+          child: Column (
+            children: [
+              const Text('Loading',
+                textDirection: TextDirection.ltr,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Courier New',
+                  fontFamilyFallback: <String>['Comic Sans'],
+                  fontSize: 50,
+                )),
+              Image.asset('images/loading_circle.gif', height:125, width:125),
+            ]
+          ),
+        );
       },
     );
   }
