e577fd0ff68370082c9aa0d6a6311c9c327e6916
Gordon D <gwd2018@gmail.com>
2022-10-31 21:56:30 -0400

    made login page scrollable

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -66,130 +66,132 @@ class _LoginPage extends State<LoginPage> {
     return MaterialApp(
       title: 'Login',
       home: Scaffold(
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-              child: Image.asset('images/beefcake_icon.jpg', height:125, width:125),
-            ),
-            const Padding(
-                padding: EdgeInsets.fromLTRB(20, 30, 20, 30),
-                child: Text('Welcome Back',
-                    textDirection: TextDirection.ltr,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Proxima Nova',
-                      fontFamilyFallback: <String>['Comic Sans'],
-                      fontSize: 38,
-                    ))),
-            Padding(
-              padding: EdgeInsets.fromLTRB(20, 0, 20, 15),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(width: 5, color: Colors.lightBlueAccent),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: SizedBox(
-                        height: 40,
-                        child: TextField(
-                          controller: usernameController,
-                          decoration: const InputDecoration(
-                            hintText: 'Email',
-                            border: InputBorder.none,
-                            prefixIcon: Icon(Icons.email_outlined, color: Colors.black),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                child: Image.asset('images/beefcake_icon.jpg', height:125, width:125),
+              ),
+              const Padding(
+                  padding: EdgeInsets.fromLTRB(20, 30, 20, 30),
+                  child: Text('Welcome Back',
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Proxima Nova',
+                        fontFamilyFallback: <String>['Comic Sans'],
+                        fontSize: 38,
+                      ))),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 15),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(width: 5, color: Colors.lightBlueAccent),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          height: 40,
+                          child: TextField(
+                            controller: usernameController,
+                            decoration: const InputDecoration(
+                              hintText: 'Email',
+                              border: InputBorder.none,
+                              prefixIcon: Icon(Icons.email_outlined, color: Colors.black),
+                            ),
+                            onChanged: ((String str) {
+                              bool valid = emailRegex.hasMatch(str);
+                              if (usernameCorrect && !valid) {
+                                print('NOT VALID ANYMORE');
+                                setState(() {usernameCorrect = false;});
+                              } else if (!usernameCorrect && valid) {
+                                print('NOW VALID BRUH');
+                                setState(() {usernameCorrect = true;});
+                              }
+                            }),
                           ),
-                          onChanged: ((String str) {
-                            bool valid = emailRegex.hasMatch(str);
-                            if (usernameCorrect && !valid) {
-                              print('NOT VALID ANYMORE');
-                              setState(() {usernameCorrect = false;});
-                            } else if (!usernameCorrect && valid) {
-                              print('NOW VALID BRUH');
-                              setState(() {usernameCorrect = true;});
-                            }
-                          }),
                         ),
                       ),
-                    ),
-                    Icon(usernameCorrect ? Icons.check_circle : Icons.flag_circle_rounded, size: 28, color: usernameCorrect ? Colors.green : Colors.red),
-                  ],
-                )
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(20, 0, 20, 15),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(width: 5, color: Colors.lightBlueAccent),
-                  borderRadius: BorderRadius.circular(5),
+                      Icon(usernameCorrect ? Icons.check_circle : Icons.flag_circle_rounded, size: 28, color: usernameCorrect ? Colors.green : Colors.red),
+                    ],
+                  )
                 ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: SizedBox(
-                        height: 40,
-                        child: TextField(
-                          decoration: const InputDecoration(
-                            hintText: 'Password',
-                            border: InputBorder.none,
-                            prefixIcon: Icon(Icons.lock_outline_rounded, color: Colors.black),
-                          ),
-                          controller: passwordController,
-                          obscureText: true,
-                          onChanged: ((String str) {
-                            bool valid = existsRegex.hasMatch(str);
-                            if (passwordCorrect && !valid) {
-                              setState(() {passwordCorrect = false;});
-                            } else if (!passwordCorrect && valid) {
-                              setState(() {passwordCorrect = true;});
-                            }
-                          })
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 15),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(width: 5, color: Colors.lightBlueAccent),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          height: 40,
+                          child: TextField(
+                            decoration: const InputDecoration(
+                              hintText: 'Password',
+                              border: InputBorder.none,
+                              prefixIcon: Icon(Icons.lock_outline_rounded, color: Colors.black),
+                            ),
+                            controller: passwordController,
+                            obscureText: true,
+                            onChanged: ((String str) {
+                              bool valid = existsRegex.hasMatch(str);
+                              if (passwordCorrect && !valid) {
+                                setState(() {passwordCorrect = false;});
+                              } else if (!passwordCorrect && valid) {
+                                setState(() {passwordCorrect = true;});
+                              }
+                            })
+                          )
                         )
                       )
-                    )
-                  ],
-                )
+                    ],
+                  )
+                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: ((){
-                if(!usernameCorrect) {
-                  setState(() {
-                    errorText = 'Invalid Email';
-                  });
-                } else if(!passwordCorrect) {
-                  setState(() {
-                    errorText = 'Enter a Password';
-                  });
-                } else {
-                  FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: usernameController.text.toString(),
-                      password: passwordController.text.toString()
-                  ).then((userCredential) =>
-                  {
-                  }).catchError((error) {
+              ElevatedButton(
+                onPressed: ((){
+                  if(!usernameCorrect) {
                     setState(() {
-                      errorText = error.message;
-                      passwordController.clear();
+                      errorText = 'Invalid Email';
                     });
-                  });
-                }
-              }),
-              child: const Text('Log In')
-            ),
-            Text(errorText, style: const TextStyle(color: Colors.red)),
-            ElevatedButton(
-              onPressed: widget.changePage,
-              child: const Text(
-                'Register with Email/Password',
-                textDirection: TextDirection.ltr
-            ))
-          ],
+                  } else if(!passwordCorrect) {
+                    setState(() {
+                      errorText = 'Enter a Password';
+                    });
+                  } else {
+                    FirebaseAuth.instance.signInWithEmailAndPassword(
+                        email: usernameController.text.toString(),
+                        password: passwordController.text.toString()
+                    ).then((userCredential) =>
+                    {
+                    }).catchError((error) {
+                      setState(() {
+                        errorText = error.message;
+                        passwordController.clear();
+                      });
+                    });
+                  }
+                }),
+                child: const Text('Log In')
+              ),
+              Text(errorText, style: const TextStyle(color: Colors.red)),
+              ElevatedButton(
+                onPressed: widget.changePage,
+                child: const Text(
+                  'Register with Email/Password',
+                  textDirection: TextDirection.ltr
+              ))
+            ],
+          ),
         ),
       ),
     );
